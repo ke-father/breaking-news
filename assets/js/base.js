a7cbdcba5aa3f0c08053c43f6585387704b8b7f0
ke <1450510686@qq.com>
2021-05-20 11:39:33 +0800

    完成了主页功能的开发

--- assets/js/base.js
+++ assets/js/base.js
@@ -2,5 +2,20 @@
 $.ajaxPrefilter(function (options) {
     // 拼接地址  根目录＋接口
     options.url = 'http://api-breakingnews-web.itheima.net' + options.url;
-    console.log(options.url);
+    // 配置请求头信息
+    if (options.url.includes('/my/')) {
+        options.headers = {
+            // 取出本地存储的token 并随着请求发出 验证
+            Authorization: localStorage.getItem('Authorization')
+        }
+    }
+    // 为请求配置 未登录跳转登录页功能
+    options.complete = function (res) {
+        if (res.responseJSON.status !== 0 || res.responseJSON.message == '身份认证失败！') {
+            // 清空本地token
+            localStorage.removeItem('Authorization');
+            // 退回到登录界面
+            location.href = '/breaking%20news/login.html'
+        }
+    }
 })
---
+++ assets/js/index.js
@@ -0,0 +1,51 @@
+$(function () {
+    getUserInfo();
+})
+
+const layer = layui.layer;
+
+// 获取用户信息
+function getUserInfo() {
+    $.ajax({
+        method: 'GET',
+        url: '/my/userinfo',
+        success: function (res) {
+            // console.log(res)
+            if (res.status !== 0) return layer.msg('登陆失败')
+            // 渲染用户头像功能
+            renderAvatar(res.data)
+        }
+
+    })
+}
+
+// // 渲染用户头像
+function renderAvatar(user) {
+    // console.log(user)
+    // 获得用户名称
+    let name = user.nickname || user.username;
+    // 渲染用户名称
+    $('#username').html(`欢迎 ${name}`);
+    // 获取用户的名称首字母
+    let first = name[0];
+    // 渲染头像
+    if (user.user_pic !== null) {
+        // 渲染图片头像
+        $('.photo').attr('src', user.user_pic).show();
+        $('.textPhoto').html(first).hide();
+    } else {
+        // 渲染文字头像
+        $('.textPhoto').html(first).show();
+        $('.photo').attr('src', user.user_pic).hide();
+    }
+}
+
+// 添加退出功能
+$('#close').on('click', function () {
+    layer.confirm('确认退出吗', {icon: 3, title:'提示'}, function(){
+       // 当点击退出清空本地储
+        localStorage.removeItem('Authorization');
+        // 跳转到登陆界面
+        location.href = '/breaking%20news/login.html'
+    });
+})
